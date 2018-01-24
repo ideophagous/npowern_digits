0a4af87f91faf9dcee305a77b69cb52e000db89c
Ideophagous <antontheog@gmail.com>
2018-01-24 12:06:34 +0000

    generalizing the number of digits

--- npowern_digits.py
+++ npowern_digits.py
@@ -9,12 +9,12 @@ def get_equal_value_index(my_list,value):
 #get the list of congruences of powers of n modulo 100,
 #as well as the index in the congruence list where the 
 #cycle breaks, and the index where the cycle starts
-def conguence_of_powers(n):
-    value = n%100
+def conguence_of_powers(n,m):
+    value = n%(10**m)
     my_list = [1,value]
     i = 2
-    while i<100:
-        value = (value*n)%100
+    while i<(10**m):
+        value = (value*n)%(10**m)
         j = get_equal_value_index(my_list,value)
         if j==-1:
             my_list.append(value)
@@ -27,20 +27,21 @@ def conguence_of_powers(n):
 
 #adjust digits to the right by adding 0, to ensure
 #we always have two digits
-def adjust_digits(k):
-    return str(k).rjust(2,'0')
+def adjust_digits(k,m):
+    return str(k).rjust(m,'0')
 
 
-#get the last two digits of n^n
-def get_npowern_digits(n):
-    cycle_end,cycle_beginning,congruences = conguence_of_powers(n)
-    n_congruence = n%100
+#get the last m digits of n^n
+def get_npowern_digits(n,m):
+    cycle_end,cycle_beginning,congruences = conguence_of_powers(n,m)
+    n_congruence = n%(10**m)
     if n<cycle_beginning:
-        return adjust_digits((n_congruence)**n%100)
+        return adjust_digits((n_congruence)**n%(10**m),m)
     else:
         actual_congruence_index = cycle_beginning+(n-cycle_beginning)%(cycle_end-cycle_beginning)
-        return adjust_digits(congruences[actual_congruence_index])
+        return adjust_digits(congruences[actual_congruence_index],m)
     
 if __name__ == '__main__':
-    n = int(input())
-    print(get_npowern_digits(n))
+    number = int(input("Enter n: "))
+    digits_count = int(input("Enter the number of digits: "))
+    print(get_npowern_digits(number,digits_count))
